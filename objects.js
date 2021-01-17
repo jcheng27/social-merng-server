feedb07ffb33f37b4a92abcdd036c8d3b7fbc8cc
jcheng27 <59032752+jcheng27@users.noreply.github.com>
2021-01-17 18:24:00 -0500

    Objects keys, values, entries, forEach , map
    Objects keys, values, entries, forEach , map

---
+++ objects.js
@@ -0,0 +1,6 @@
+users = { 'a': 'amy', 'b':'brie', 'c':'christie' }
+Object.keys(users).forEach((keys, index) => console.log(index, keys));
+Object.values(users).forEach((values, index) => console.log(index,values));
+Object.entries(users).forEach((values, index) => console.log(index, values));
+
+myObj = Object.entries(users).map(values => {return values[0] + ' is for ' + values[1]})
