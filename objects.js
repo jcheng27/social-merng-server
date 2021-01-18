6f3da424cd8ae9615d61391316ef51ad00b68f6d
jcheng27 <59032752+jcheng27@users.noreply.github.com>
2021-01-17 20:42:12 -0500

    Object fromEntries
    Object fromEntries

--- objects.js
+++ objects.js
@@ -4,3 +4,6 @@ Object.values(users).forEach((values, index) => console.log(index,values));
 Object.entries(users).forEach((values, index) => console.log(index, values));
 
 myObj = Object.entries(users).map(values => {return values[0] + ' is for ' + values[1]})
+
+players = [['d','diane'], ['e','elise'], ['f','faye']]
+Object.fromEntries(players)
