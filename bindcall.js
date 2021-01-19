15827c5e8c7611ff793233455e455b107982078c
jcheng27 <59032752+jcheng27@users.noreply.github.com>
2021-01-19 14:31:21 -0500

    This Bind Call in Object
    understanding This Bind Call in an Object

---
+++ bindcall.js
@@ -0,0 +1,23 @@
+// https://www.javascripttutorial.net/javascript-bind/
+// https://www.codingame.com/playgrounds/9799/learn-solve-call-apply-and-bind-methods-in-javascript
+// https://dev.to/thesanjeevsharma/call-apply-and-bind-in-javascript-2nno
+
+var car = { 
+    registrationNumber: "GA12345",
+    brand: "Toyota",
+
+    displayDetails: function(ownerName){
+        console.log(ownerName + ", this is your car: " + this.registrationNumber + " " + this.brand);
+    }
+}
+
+const jethroCar = { registrationNumber: "CHJZ782", brand: "Honda" }
+
+const dadCar = { registrationNumber: "AAPF523", brand: "Accord"}
+
+//Bind returns a function
+mycar = car.displayDetails.bind(jethroCar, "Jethro")
+mycar()
+
+//Call automatically returns
+car.displayDetails.call(dadCar, "Dad")
